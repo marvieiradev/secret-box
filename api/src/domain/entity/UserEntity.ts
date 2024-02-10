0ae409b286e192b0d9dd599d88db9c79b06720c5
Marcos Vieira <marcosvieira482@gmail.com>
2024-02-10 18:19:11 -0300

    feat: create User model

--- api/src/domain/entity/UserEntity.ts
+++ api/src/domain/entity/UserEntity.ts
@@ -1,3 +1,15 @@
 export default class UserEntity {
-    constructor() { }
+    constructor(
+        readonly userId: number,
+        readonly name: string | null,
+        readonly email: string,
+        readonly password: string,
+        readonly pictureUrl: string | null,
+        readonly createdAt: Date,
+        readonly updatedAt: Date,
+    ) {
+    }
+    static create(email: string, password: string): UserEntity {
+        return new UserEntity(0, null, email, password, null, new Date(), new Date)
+    }
 }
