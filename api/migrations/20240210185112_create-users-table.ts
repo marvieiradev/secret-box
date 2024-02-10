bdb043f1aae64f9aa2db0172774adf1c5acdcd3b
Marcos Vieira <marcosvieira482@gmail.com>
2024-02-10 16:19:16 -0300

    feat: create users table in the first migration

--- api/migrations/20240210185112_create-users-table.ts
+++ api/migrations/20240210185112_create-users-table.ts
@@ -3,12 +3,19 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     //criar no banco de dados a tabela user
-    /*return knex.schema.createTable('users', (table) => {
-        table.uuid('userId')
-    });*/
+    return knex.schema.createTable('users', (table) => {
+        table.uuid('userId').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.text('password').notNullable();
+        table.timestamp('createdAt').defaultTo(knex.fn.now());
+        table.timestamp('updatedAt').notNullable();
+    });
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    //deletar a tabela user do banco de dados 
+    return knex.schema.dropTable('users');
 }
 
