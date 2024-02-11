29f10c5ddace1d19daf0c5906933742488577225
Marcos Vieira <marcosvieira482@gmail.com>
2024-02-11 11:38:15 -0300

    feat: add initial configuration for the database connection

---
+++ api/src/infra/database/KnexAdapter.ts
@@ -0,0 +1,51 @@
+import knex, { Knex as KnexType } from "knex";
+import { AnswerModel, QuestionModel, UserModel } from "@domain/model";
+import DatabaseConnection from "./DatabaseConnection";
+
+export default class KnexAdapter implements DatabaseConnection {
+    private connection: KnexTypeAdapter;
+
+    constructor() {
+        this.connection = {} as KnexTypeAdapter;
+    }
+
+    connect(): Promise<void> {
+        try {
+            this.connection = knex({
+                client: 'pg',
+                connection: {
+                    host: process.env.DB_HOST,
+                    user: process.env.DB_USERNAME,
+                    password: process.env.DB_PASSWORD,
+                    database: process.env.DB_NAME,
+                    port: 5432,
+                }
+            });
+            return Promise.resolve();
+        } catch (error) {
+            throw new Error('Error connecting to database.');
+        }
+    }
+
+    disconnect(): Promise<void> {
+        return this.connection.destroy();
+    }
+
+    get instance(): KnexType {
+        return this.connection;
+    }
+}
+
+interface DatabaseTables {
+    answers: AnswerModel;
+    questions: QuestionModel;
+    users: UserModel;
+}
+
+export enum DatabaseTableNames {
+    ANSWERS = 'answers',
+    QUESTIONS = 'questions',
+    USERS = 'users',
+}
+
+type KnexTypeAdapter = KnexType<DatabaseTables>;
