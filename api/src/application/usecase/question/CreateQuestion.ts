d9106be8842491af73eafae041377a85a4bb3144
Marcos Vieira <marcosvieira482@gmail.com>
2024-02-12 12:01:07 -0300

    feat: creation of the QuestionRepository and Questions use cases

---
+++ api/src/application/usecase/question/CreateQuestion.ts
@@ -0,0 +1,33 @@
+import QuestionRepository from "@application/repository/QuestionRepository";
+import QuestionEntity from "@domain/entity/QuestionEntity";
+import Registry from "@infra/di/Registry";
+
+export default class CreateQuestion {
+    private readonly questionRepository: QuestionRepository;
+    constructor() {
+        const registry = Registry.getInstance();
+        this.questionRepository = registry.resolve<QuestionRepository>('QuestionRepository');
+    }
+
+    async execute(questionData: InputCreateQuestion): Promise<OutputCreateQuestion> {
+        const question = QuestionEntity.create(questionData.question, questionData.userId);
+        await this.questionRepository.create(question);
+
+        return {
+            questionId: question.questionId,
+            question: question.question,
+            createdAt: question.createdAt,
+        };
+    }
+}
+
+type InputCreateQuestion = {
+    question: string;
+    userId: string;
+}
+
+type OutputCreateQuestion = {
+    questionId: string;
+    question: string;
+    createdAt: Date;
+}
