653537eaac9805e7b7864bb29ae667a1fd72c9c9
Marcos Vieira <marcosvieira482@gmail.com>
2024-02-12 10:51:39 -0300

    feat: create api/user route

--- api/src/infra/http/ExpressAdapter.ts
+++ api/src/infra/http/ExpressAdapter.ts
@@ -3,6 +3,7 @@ import express from "express";
 import "express-async-errors";
 import bodyParser from "body-parser";
 import cors from "cors";
+import UserController from "@infra/http/controller/UserControllerImpl";
 
 export default class ExpressAdapter implements HttpServer {
     readonly app: express.Application;
@@ -11,10 +12,17 @@ export default class ExpressAdapter implements HttpServer {
         this.app = express();
         this.app.use(cors());
         this.app.use(bodyParser.json());
+
+        const userController = new UserController();
+
+        this.app.use('/api/user', userController.create);
+
         this.app.use('/api', (req, res) => {
             res.json({ message: 'Hello!' });
         })
     }
+
+
 
     listen(port: number): void {
         this.app.listen(port, () => {
