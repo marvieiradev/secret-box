47b26eb503451d12775829018250b4c4507dd5c1
Marcos Vieira <marcosvieira482@gmail.com>
2024-02-12 16:10:11 -0300

    feat: new migration

---
+++ api/migrations/20240212180527_update-table-field-answers.ts
@@ -0,0 +1,15 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.table('answers', (table) => {
+        table.uuid('userId').nullable().alter();
+    })
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.table('answers', (table) => {
+        table.uuid('userId').notNullable().alter();
+    })
+}
+
